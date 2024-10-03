d2741489541f522f6862ace23ccced555a1bba21
Federico Di Presa <federicodipresa94@gmail.com>
2024-10-04 00:57:16 +0200

    continuazione costruzione PlayPage

--- front-end/src/components/react/PlayPage.jsx
+++ front-end/src/components/react/PlayPage.jsx
@@ -13,11 +13,52 @@ const PlayPage = () => {
   const [selectedHops, setSelectedHops] = useState([]);
   const [selectedYeast, setSelectedYeast] = useState(null);
 
-  const maltsOptions = ["Malto Chiaro", "Malto Scuro", "Malto Rosso"];
-  const hopsOptions = ["Luppolo Amarillo", "Luppolo Cascade", "Luppolo Simcoe"];
-  const yeastOptions = ["Lievito Ale", "Lievito Lager"];
+  // Opzioni per i malti
+  const maltsOptions = [
+    {
+      type: "Malto Chiaro",
+      malts: ["Malto Wheat", "Pilsner", "Pale Ale"],
+    },
+    {
+      type: "Malto Ambrato",
+      malts: ["Vienna", "Monaco", "Caramel"],
+    },
+    {
+      type: "Malto Scuro",
+      malts: ["Abbey", "Carafa III", "Chocolate"],
+    },
+  ];
+
+  // Opzioni per i luppoli
+  const hopsOptions = [
+    {
+      type: "Luppoli Base",
+      hops: ["Saaz", "Styrian Golding", "Hallertau Magnum"],
+    },
+    {
+      type: "Luppoli Agrumati",
+      hops: ["Citra", "Hbc-630", "Wakatu"],
+    },
+    {
+      type: "Luppoli Fruttati",
+      hops: ["Wai-ti", "Mosaic", "Vic Secret"],
+    },
+  ];
+
+  // Opzioni per i lieviti
+  const yeastOptions = [
+    {
+      type: "Lievito Ale (Alta Fermentazione)",
+      yeast: ["US-05"],
+    },
+    {
+      type: "Lievito lager ( Bassa Fermentazione)",
+      yeast: ["Saflager w34/70"],
+    },
+  ];
 
   const toggleRegole = () => setRegoleOpen(!regoleOpen);
+
   const toggleDropdown = (type) => {
     if (type === "malts") setShowMaltsDropdown(!showMaltsDropdown);
     if (type === "hops") setShowHopsDropdown(!showHopsDropdown);
@@ -46,7 +87,7 @@ const PlayPage = () => {
 
   const getBeerColor = () => {
     if (selectedMalts.includes("Malto Scuro")) return "brown";
-    if (selectedMalts.includes("Malto Rosso")) return "red";
+    if (selectedMalts.includes("Malto Ambrato")) return "amber";
     return "gold";
   };
 
@@ -71,18 +112,30 @@ const PlayPage = () => {
               {regoleOpen && (
                 <ul className="dropdown-menu show">
                   <li>
-                    <a className="dropdown-item" href="#">
-                      Regola 1
+                    <a className="dropdown-item">
+                      1. Selezionare i <strong>malti</strong> che si vogliono
+                      inserire nella <br /> ricetta cliccando sul pulsante
+                      <strong> Malto</strong>. &#40; Max. 3 &#41;
                     </a>
                   </li>
                   <li>
-                    <a className="dropdown-item" href="#">
-                      Regola 2
+                    <a className="dropdown-item">
+                      2. Selezionare i <strong>luppoli</strong> che si vogliono
+                      inserire nella <br /> ricetta cliccando sul pulsante{" "}
+                      <strong> Luppoli</strong>. &#40; Max. 4 &#41;
                     </a>
                   </li>
                   <li>
-                    <a className="dropdown-item" href="#">
-                      Regola 3
+                    <a className="dropdown-item">
+                      3. Scegliere uno dei due <strong>lieviti</strong> proposti
+                      cliccando sul
+                      <br /> pulsante <strong> Lievito</strong>. &#40; Max. 1
+                      &#41;
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item">
+                      4. Premere <strong>Genera</strong>!
                     </a>
                   </li>
                 </ul>
@@ -105,16 +158,23 @@ const PlayPage = () => {
             {showMaltsDropdown && (
               <div className="dropdown-content show">
                 {maltsOptions.map((malto) => (
-                  <a
-                    key={malto}
-                    href="#"
-                    onClick={() => handleMaltsChange(malto)}>
-                    {malto}
-                  </a>
+                  <div key={malto.type}>
+                    <strong>{malto.type}</strong>
+                    <ul>
+                      {malto.malts.map((m) => (
+                        <li key={m}>
+                          <a href="#" onClick={() => handleMaltsChange(m)}>
+                            {m}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
                 ))}
               </div>
             )}
           </div>
+
           <div className="dropdown">
             <button
               className="btn hops-btn"
@@ -123,17 +183,24 @@ const PlayPage = () => {
             </button>
             {showHopsDropdown && (
               <div className="dropdown-content show">
-                {hopsOptions.map((luppolo) => (
-                  <a
-                    key={luppolo}
-                    href="#"
-                    onClick={() => handleHopsChange(luppolo)}>
-                    {luppolo}
-                  </a>
+                {hopsOptions.map((hop) => (
+                  <div key={hop.type}>
+                    <strong>{hop.type}</strong>
+                    <ul>
+                      {hop.hops.map((h) => (
+                        <li key={h}>
+                          <a href="#" onClick={() => handleHopsChange(h)}>
+                            {h}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
                 ))}
               </div>
             )}
           </div>
+
           <div className="dropdown">
             <button
               className="btn yeast-btn"
@@ -142,13 +209,19 @@ const PlayPage = () => {
             </button>
             {showYeastDropdown && (
               <div className="dropdown-content show">
-                {yeastOptions.map((lievito) => (
-                  <a
-                    key={lievito}
-                    href="#"
-                    onClick={() => handleYeastChange(lievito)}>
-                    {lievito}
-                  </a>
+                {yeastOptions.map((yeast) => (
+                  <div key={yeast.type}>
+                    <strong>{yeast.type}</strong>
+                    <ul>
+                      {yeast.yeast.map((y) => (
+                        <li key={y}>
+                          <a href="#" onClick={() => handleYeastChange(y)}>
+                            {y}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
                 ))}
               </div>
             )}
