5bdab75d33db355c52ec448404beca780aee406e
Federico Di Presa <federicodipresa94@gmail.com>
2024-10-21 15:57:51 +0200

    Update ResultPage.jsx

--- front-end/src/components/ResultPage/ResultPage.jsx
+++ front-end/src/components/ResultPage/ResultPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../ResultPage/ResultPage.css";
 import "../App/App.css";
